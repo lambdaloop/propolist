cc54f25c3b36824d64fbb36a8867b7f37c06a3be
Pierre Karashchuk <krchtchk@gmail.com>
2013-03-09 11:47:12 -0800

    blah

--- Handler/List.hs
+++ Handler/List.hs
@@ -13,15 +13,23 @@ import Control.Applicative
 -- listWidget = do
 --     toWidget [hamlet|
 -- #{thmCategory $ head thms}
--- |]
+-- ]
 
 -- | Handler for /list, the list of theorems added.
 getListR :: Handler RepHtml
 getListR = do
     thms <- runDB $ selectList ([] :: [Filter Thm]) [LimitTo 10]
-    defaultLayout [whamlet|
-<h1> [Added Theorem] <br>
-$forall Entity thmId thm <- thms
-    <p>#{thmCategory thm}
-    <p>#{thmContent thm}
-|]
+    defaultLayout $
+       [whamlet|
+ <h1> [Theorem] <br>
+ $forall Entity thmId thm <- thms
+    <p>
+      <h4> #{show $ thmName thm}
+      <h5> #{show $ thmCategory thm}
+      <p style="font-size:medium;"> #{thmContent thm}
+      <p> #{show $ thmProof thm}
+      <p> #{show $ thmName thm}
+      <p> #{show $ thmSign thm}
+      <p> #{show $ thmRef thm}
+      <p> #{show $ thmNote thm}
+ |]
