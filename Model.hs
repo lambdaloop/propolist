caac6297f0960d4145c17cd4b609e1efc68cb134
Pierre Karashchuk <krchtchk@gmail.com>
2013-03-09 00:18:50 -0800

    got primitive database set up

--- Model.hs
+++ Model.hs
@@ -4,7 +4,43 @@ import Prelude
 import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
+import Data.Time (Day)
 
+
+-- | Internal Thm Type, representing a theorem entity.
+-- data Thm = Thm
+--   { thmType ::          Maybe ThmType
+--   , thmContent ::       Maybe ThmContent
+--   , thmProof ::         Maybe ThmProof
+--   , thmName ::          Maybe ThmName
+--   , thmSignature ::     Maybe ThmSignature
+--   , thmRef ::           Maybe ThmRef -- references; e.g. urls, paper title, book section
+--   , thmNote ::          Maybe ThmNote -- notes
+--   } deriving (Eq, Show)
+
+data ThmCategory = Corrolary
+             | Definition
+             | Theorem
+             | Proposition
+             | Lemma
+             | Algorithm
+             | Remark
+             deriving (Show, Eq, Read)
+derivePersistField "ThmCategory"
+
+-- ThmSignature  From To
+-- Examples:
+-- Reals -> ModEquation
+-- Equation -> Equation
+-- data ThmSignature = ThmSignature [ThmStatement] [ThmStatement] deriving (Show, Eq)
+
+type ThmContent = Text
+type ThmName = Text
+type ThmProof = Text
+type ThmRef = Text
+type ThmNote = Text
+
+type ThmStatement = Text
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
--- Types.hs
+++ Types.hs
@@ -1,38 +1,38 @@
 module Types where
 
-import qualified Data.Text as T (Text)
+import Data.Text (Text)
 import Prelude
 
 -- | Internal Thm Type, representing a theorem entity.
-data Thm = Thm
-  { thmType ::          Maybe ThmType
-  , thmContent ::       Maybe ThmContent
-  , thmProof ::         Maybe ThmProof
-  , thmName ::          Maybe ThmName
-  , thmSignature ::     Maybe ThmSignature
-  , thmRef ::           Maybe ThmRef -- references; e.g. urls, paper title, book section
-  , thmNote ::          Maybe ThmNote -- notes
-  } deriving (Eq, Show)
+-- data Thm = Thm
+--   { thmType ::          Maybe ThmType
+--   , thmContent ::       Maybe ThmContent
+--   , thmProof ::         Maybe ThmProof
+--   , thmName ::          Maybe ThmName
+--   , thmSignature ::     Maybe ThmSignature
+--   , thmRef ::           Maybe ThmRef -- references; e.g. urls, paper title, book section
+--   , thmNote ::          Maybe ThmNote -- notes
+--   } deriving (Eq, Show)
 
-data ThmType = Corrolary
-             | Definition
-             | Theorem
-             | Proposition
-             | Lemma
-             | Algorithm
-             | Remark
-             deriving (Show, Eq)
+-- data ThmType = Corrolary
+--              | Definition
+--              | Theorem
+--              | Proposition
+--              | Lemma
+--              | Algorithm
+--              | Remark
+--              deriving (Show, Eq)
 
-                    -- ThmSignature  From To
-                    -- Examples:
-                    -- Reals -> ModEquation
-                    -- Equation -> Equation
-data ThmSignature = ThmSignature [ThmStatement] [ThmStatement] deriving (Show, Eq)
+--                     -- ThmSignature  From To
+--                     -- Examples:
+--                     -- Reals -> ModEquation
+--                     -- Equation -> Equation
+-- data ThmSignature = ThmSignature [ThmStatement] [ThmStatement] deriving (Show, Eq)
 
-type ThmContent = T.Text
-type ThmName = T.Text
-type ThmProof = T.Text
-type ThmRef = T.Text
-type ThmNote = T.Text
+-- type ThmContent = T.Text
+-- type ThmName = T.Text
+-- type ThmProof = T.Text
+-- type ThmRef = T.Text
+-- type ThmNote = T.Text
 
-type ThmStatement = T.Text
+-- type ThmStatement = T.Text
