bc4e086111c0cf4d61ce3fd4d007e6379d7c4f4c
Zongheng Yang <concretevitamin@gmail.com>
2013-03-08 16:18:59 -0800

    Types: Slight additions.

--- Types.hs
+++ Types.hs
@@ -1,21 +1,36 @@
-data ThmType = Corrolary | Definition | Theorem | Proposition | Lemma | Algorithm
-type ThmStatement = String
+module Types where
+
+import qualified Data.Text as T (Text)
+
+-- | Internal Thm Type, representing a theorem entity.
+data Thm = Thm
+  { thmType ::          ThmType
+  , thmContent ::       ThmContent
+  , thmName ::          Maybe ThmName
+  , thmSignature ::     Maybe ThmSignature
+  , thmProof ::         Maybe ThmProof
+  , thmRef ::           Maybe ThmRef -- references; e.g. urls, paper title, book section
+  , thmNote ::          Maybe ThmNote -- notes
+  }
+
+data ThmType = Corrolary
+             | Definition
+             | Theorem
+             | Proposition
+             | Lemma
+             | Algorithm
+             | Remark
 
                     -- ThmSignature  From To
                     -- Examples:
                     -- Reals -> ModEquation
                     -- Equation -> Equation
 data ThmSignature = ThmSignature [ThmStatement] [ThmStatement]
-type ThmContent = String
-type ThmProof = String
-type ThmRef = String
 
-data Thm =
-  { thmType ::          ThmType
-  , thmContent ::       ThmContent
+type ThmContent = T.Text
+type ThmName = T.Text
+type ThmProof = T.Text
+type ThmRef = T.Text
+type ThmNote = T.Text
 
-  , thmName ::          Maybe String
-  , thmSignature ::     Maybe ThmSignature
-  , thmProof ::         Maybe ThmProof
-  , thmRef ::           Maybe ThmRef
-  }
+type ThmStatement = T.Text
